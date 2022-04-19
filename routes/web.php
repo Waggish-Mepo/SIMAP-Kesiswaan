d080634b64544cacd38534f6e2959a679d1bab68
Muhamad Lutfi Azizan <mlutfiazizan@gmail.com>
2022-04-20 00:34:01 +0700

    crud razia & barang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RekapBarangController;
+use App\Http\Controllers\RekapBarang\BarangRaziaController;
+use App\Http\Controllers\RekapBarang\BarangTemuanController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,20 +27,19 @@ Route::get('/dashboard', function(){
     return view('dashboard');
 });
 
-Route::get('/rekap-barang/razia', function(){
-    return view('rekap-barang.razia.index');
-});
+Route::prefix('/rekap-barang')->group(function(){
+    Route::get('/razia', [BarangRaziaController::class, 'index'])->name('razia.index');
+    Route::post('/razia', [BarangRaziaController::class, 'store'])->name('razia.store');
+    Route::put('/razia/{id}', [BarangRaziaController::class, 'update'])->name('razia.update');
+    Route::delete('/razia/{id}', [BarangRaziaController::class, 'destroy'])->name('razia.destroy');
+    Route::put('/razia/{id}/kembali',[BarangRaziaController::class, 'kembali'])->name('razia.kembali');
+    Route::put('/razia/{id}/razia',[BarangRaziaController::class, 'razia'])->name('razia.razia');
+    Route::get('/razia/details/{nis}', [BarangRaziaController::class, 'getDetails'])->name('razia.getDetails');
 
-Route::get('/rekap-barang/razia/edit', function(){
-    return view('rekap-barang.modals.modal-razia.edit');
-});
-
-Route::get('/rekap-barang/temuan', function(){
-    return view('rekap-barang.temuan.index');
-});
-
-Route::get('/rekap-barang/temuan/edit', function(){
-    return view('rekap-barang.modals.modal-temuan.edit');
+    Route::get('/temuan', [BarangTemuanController::class, 'index'])->name('temuan.index');
+    Route::post('/temuan', [BarangTemuanController::class, 'store'])->name('temuan.store');
+    Route::put('/temuan/{id}', [BarangTemuanController::class, 'update'])->name('temuan.update');
+    Route::delete('/temuan/{id}', [BarangTemuanController::class, 'destroy'])->name('temuan.destroy');
 });
 
 Route::get('/warning-letter', function(){
